bdf5f8ee0e889d224c35ac01b14af39eaa134272
ji3ee <jieee1602@gmail.com>
2026-05-08 09:25:25 +0700

    Refactor code structure for improved readability and maintainability

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Kawung Pitu Institute - Promoting Sustainable Livelihood' }}</title>
 
-    <link rel="icon" type="image/png" href="{{ asset('logo-kawung-ori.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('kawung.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
